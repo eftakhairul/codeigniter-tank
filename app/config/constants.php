eaa336eed929ea3b1eeb4b16a9fca0c614fa304a
Eftakhairul Islam <eftakhairul@gmail.com>
2013-11-14 01:46:11 -0500

    config/constant.php is updated again

--- app/config/constants.php
+++ app/config/constants.php
@@ -37,5 +37,67 @@ define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 
+
+/*
+|--------------------------------------------------------------------------
+| Twitter Setting
+|--------------------------------------------------------------------------
+|
+| These modes are used when working with fopen()/popen()
+|
+*/
+include_once (APPPATH . '/config/servers.php');
+$active_group = getCurrentServer();
+
+
+switch ($active_group) {
+    case 'staging':
+        define("CONSUMER_KEY",      "");
+        define("CONSUMER_SECRET",   "");
+        define("OAUTH_TOKEN",       "");
+        define("OAUTH_SECRET",      "");
+        break;
+
+    case 'production':
+        define("CONSUMER_KEY",      "");
+        define("CONSUMER_SECRET",   "");
+        define("OAUTH_TOKEN",       "");
+        define("OAUTH_SECRET",      "");
+        break;
+
+    case 'default':
+        define("CONSUMER_KEY",      "");
+        define("CONSUMER_SECRET",   "");
+        define("OAUTH_TOKEN",       "");
+        define("OAUTH_SECRET",      "");
+        break;
+}
+
+/*
+|--------------------------------------------------------------------------
+| Facebook and Twitter Setting
+|--------------------------------------------------------------------------
+|
+| Facebook and Twitter setting is loaded based on server environment
+|
+*/
+switch ($active_group) {
+    case 'staging':
+        define("fb_app_id",      "");
+        define("fb_app_secret",  "");
+        break;
+
+    case 'production':
+        define("fb_app_id",      "");
+        define("fb_app_secret",  "");
+        break;
+
+    case 'default':
+        define("fb_app_id",      "");
+        define("fb_app_secret",  "");
+        break;
+}
+
+
 /* End of file constants.php */
 /* Location: ./app/config/constants.php */
